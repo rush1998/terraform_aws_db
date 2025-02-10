177106e3ca1d0eacacbce628a6be61c5196c4030
rush1998 <rushabh.m.patel1998@gmail.com>
2025-02-09 22:07:15 -0500

    second commit

--- rds.tf
+++ rds.tf
@@ -1,12 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  allocated_storage    = 10
   db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
   username             = "foo"
-  password             = "${file("../pass.txt")}"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+  password             = file("../pass.txt")
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
 }
